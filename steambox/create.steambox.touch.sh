29bbedf7e7397cea42f9105d9348fd538cfb18f7
maxhougas <max.hougas@gmail.com>
2026-01-02 12:05:35 -0600

    added steam-toucher

---
+++ steambox/create.steambox.touch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+$MOUNT="$(realpath $0 | grep -o '^.*/')mount"
+
+docker create \
+ -v $MOUNT/creds:/home/user/creds \
+ --name steamtouch maxhougas/steambox:db \
+ su -c '\
+  ~/steamcmd.sh +login $(cat home/user/creds) +quit \
+ ' user 
