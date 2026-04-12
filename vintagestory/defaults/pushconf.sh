f532a20045e1ec51a2f56edbfd958e5d74fa8080
maxhougas <max.hougas@gmail.com>
2026-04-11 21:40:27 -0500

    updates to file structure

--- vintagestory/defaults/pushconf.sh
+++ vintagestory/defaults/pushconf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 DIR=$(realpath $0 | grep -o '.*/')
-cp "$DIR"serverconfig.json ../mount/serverconfig.json
+cp "$DIR"serverconfig.json "$DIR"../mount/serverconfig.json
 chmod 666 ../mount/serverconfig.json
